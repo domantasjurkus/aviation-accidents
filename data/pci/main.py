87bdc3d5e9a04bf52b7c57a0c39b2a9728d186d3
Domantas Jurkus <domantas.jurkus@gmail.com>
2017-04-23 18:54:33 +0000

    Work on PCI scrapping

--- data/pci/main.py
+++ data/pci/main.py
@@ -1,8 +1,29 @@
 from bs4 import BeautifulSoup
 import urllib2
+import re
 
+import attributes
+
+ROOT_URL = 'http://www.planecrashinfo.com'
+
+# TODO
+'''def create_schema(conn, db_path):
+	cursor = conn.cursor()
+	cursor.execute('DROP TABLE IF EXISTS pci;')
+
+	query = 'CREATE TABLE IF NOT EXISTS pci('
+
+	for e in attributes.attrs:
+		# Skip ignored attributes
+		if e['label'] in attributes.ignore:
+			continue
+		query += e['label']+' '+e['sql_attr']
+	query = query[:-1]+')'
+
+	cursor.execute(query)'''
+
+# Returns an array of incident details
 def get_accident_details(url):
-    print 'Fetching page'
     html = urllib2.urlopen(url).read()
     soup = BeautifulSoup(html, 'html.parser')
     rows = soup.table.find_all('tr')
@@ -14,14 +35,48 @@ def get_accident_details(url):
         rows[5].find_all('td')[1].string, # flight_no
         rows[6].find_all('td')[1].string, # route
         rows[7].find_all('td')[1].string, # ac_type
-        rows[8].find_all('td')[1].string  # registration
+        rows[8].find_all('td')[1].string,  # registration
+        rows[9].find_all('td')[1].string,  # construction or serial number / Line or fuselage number
+        rows[10].find_all('td')[1].string,  # passengers aboard
+        rows[11].find_all('td')[1].string,  # fatalities
+        rows[12].find_all('td')[1].string,  # ground fatalities
+        rows[13].find_all('td')[1].string  # summary
     ]
     
-    print rtn
+    # Clean the data
+    for i in range(len(rtn)):
+        # Replace ? with None
+        rtn[i] = None if rtn[i] == '?' else rtn[i]
+        # TODO: Extract passenger and crew member count
+        if i == 9:
+            found = re.search('(?<=passengers:)', rtn[i])
+            if not found:
+                print 'ERROR scrapping passenger count'
+                return
+            print found
+    
+    return rtn
+    
+# Scrape a single year
+def get_accident_urls(year):
+    if year < 1920:
+        return
+    
+    url = 'http://www.planecrashinfo.com/%s/%s.htm' % (year, year)
+    html = urllib2.urlopen(url).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    rows = soup.table.find_all('tr')[1:]
+    for r in rows:
+        href = r.find_all('a')[0]['href']
+        accident_url = '%s/%s/%s' % (ROOT_URL, year, href)
+        print get_accident_details(accident_url)
 
 def main():
-    url = 'http://www.planecrashinfo.com/2001/2001-42.htm'
-    get_accident_details(url)
+    url1 = ROOT_URL+'/2001/2001-42.htm'
+    url2 = ROOT_URL+'/1922/1922-7.htm'
+    #get_accident_urls(1922)
+    get_accident_details(url2)
+    
     
 if __name__ == '__main__':
     main()
